fd6515ebe17d196520e00fca15473980333f516d
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-10-17 01:36:44 +0530

    deployed

--- src/webpage_component/Contact.jsx
+++ src/webpage_component/Contact.jsx
@@ -1,14 +1,74 @@
 import React from "react";
 import "./style.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button } from "react-bootstrap";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "mywebsite_contact_page",
+        e.target,
+        "user_ZZHUyeeMX1AmQhztiqhC3"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+
+    alert("sent");
+  }
   return (
     <>
       <div id="webpage">
         <div id="webpage_body">
-          <h1 id="title" data-aos="fade-right">
-            SAYAIB SARKAR
-          </h1>
+          <div className="container">
+            <Form onSubmit={sendEmail}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter name"
+                  name="name"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="email"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Subject</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="subject"
+                  name="subject"
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" name="message" rows={3} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Send Message
+              </Button>
+            </Form>
+          </div>
         </div>
       </div>
     </>
