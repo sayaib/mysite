fc85555d5105e1051f744cf8732647f12f019812
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-27 20:52:07 +0530

    update minor styles

--- src/webpage_component/Contact.jsx
+++ src/webpage_component/Contact.jsx
@@ -33,32 +33,22 @@ const Contact = () => {
         <div id="webpage_body">
           <div className="container">
             <h1 className="contact_heading">Contact</h1>
-            <p className="contact_subline">For all enquiries, please email me using the form below.</p>
-            <h3 className="form_upline">How can I help you?</h3>
+            <p className="contact_subline">
+              For all enquiries, please email me using the form below.
+            </p>
+            <h4 className="form_upline">How can I help you?</h4>
             <Form onSubmit={sendEmail}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter name"
-                  name="name"
-                />
+                <Form.Control type="text" name="name" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  name="email"
-                />
+                <Form.Control type="email" name="email" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Subject</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="subject"
-                  name="subject"
-                />
+                <Form.Control type="text" name="subject" />
               </Form.Group>
               <Form.Group
                 className="mb-3"
@@ -67,7 +57,7 @@ const Contact = () => {
                 <Form.Label>Message</Form.Label>
                 <Form.Control as="textarea" name="message" rows={3} />
               </Form.Group>
-              <Button variant="primary" type="submit">
+              <Button variant="success" type="submit">
                 Send Message
               </Button>
             </Form>
